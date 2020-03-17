eee70d6a02416913c2c081dd761a940ae8474688
Maveric4 <danielgacek97@gmail.com>
2020-03-17 16:27:47 +0000

    web streaming

--- python_scripts/web_listen.py
+++ python_scripts/web_listen.py
@@ -1,26 +1,28 @@
 import socket
-import time
-import picamera
+import subprocess
 
-# Connect a client socket to my_server:8000 (change my_server to the
-# hostname of your server)
-client_socket = socket.socket()
-client_socket.connect(('192.168.9.192', 8000))
+# Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
+# all interfaces)
+server_socket = socket.socket()
+server_socket.bind(('localhost', 8000))
+server_socket.listen(0)
 
-# Make a file-like object out of the connection
-connection = client_socket.makefile('wb')
+# Accept a single connection and make a file-like object out of it
+connection = server_socket.accept()[0].makefile('rb')
 try:
-    camera = picamera.PiCamera()
-    camera.resolution = (640, 480)
-    camera.framerate = 24
-    # Start a preview and let the camera warm up for 2 seconds
-    camera.start_preview()
-    time.sleep(2)
-    # Start recording, sending the output to the connection for 60
-    # seconds, then stop
-    camera.start_recording(connection, format='h264')
-    camera.wait_recording(60)
-    camera.stop_recording()
+    # Run a viewer with an appropriate command line. Uncomment the mplayer
+    # version if you would prefer to use mplayer instead of VLC
+    cmdline = ['vlc', '--demux', 'h264', '-']
+    #cmdline = ['mplayer', '-fps', '25', '-cache', '1024', '-']
+    player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+    while True:
+        # Repeatedly read 1k of data from the connection and write it to
+        # the media player's stdin
+        data = connection.read(1024)
+        if not data:
+            break
+        player.stdin.write(data)
 finally:
     connection.close()
-    client_socket.close()
+    server_socket.close()
+    player.terminate()
